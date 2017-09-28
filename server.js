a73294352bdb43e037681b5a1aeb9507a5a3947c
Bruce <brucegraham11@gmail.com>
2017-09-28 10:14:45 -0700

    Add process.env.PORT || 3000 on app.listen() and rename of some variables

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const app = express();
 const axios = require('axios');
 const nodemailer = require('nodemailer');
 const schedule = require('node-schedule');
 const config = require('./config.js');
+const app = express();
 
-const getDataAndEmail = () => {
+const getDataSendEmail = () => {
   axios.get('http://api.sportradar.us/nascar-ot3/mc/2017/standings/drivers.json?api_key=' + config.sportradarKey)
-    .then(function (response) {
+    .then(response => {
       const data = response.data;
 
       const topDrivers = data.drivers.filter(driver => {
@@ -30,11 +30,11 @@ const getDataAndEmail = () => {
       });
 
       const mailOptions = {
-        from: config.fromEmail,
+        from: config.emailDescription,
         to: config.toEmail,
         subject: "This Weeks Racing Picks",
         text: topDrivers
-      }
+      };
 
       transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
@@ -42,15 +42,15 @@ const getDataAndEmail = () => {
         }
         console.log('Message sent: %s', info.messageId);
       });
-
     })
     .catch(function (error) {
       console.log(error);
     });
 };
 
+// schedule.scheduleJob('59 * * * * *', getDataAndEmail);
 schedule.scheduleJob('* * 12 * * 5', getDataAndEmail);
 
-app.listen('3333', () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Listening on port 3000');
 });
